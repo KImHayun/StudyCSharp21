1984b1e8886cb3f22fd9f5b539afc8edb86ba84f
KimHayun <95-ha@naver.com>
2021-02-23 17:27:09 +0900

    2월23일

---
+++ chap05/InfiniteLoopApp/InfiniteLoopApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InfiniteLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            long idx = 0;
+            // for(; ; ) //무한루프
+            while (true)
+                {
+                Console.WriteLine($"idx={idx++}");
+            }
+        }
+    }
+}
---
+++ chap06/Chap06App/Chap06App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap06App
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            int x = Calculator.Plus(3, 4);
+            int y = Calculator.Plus(5, 6);
+            int z = Calculator.Plus(7, 8);
+
+
+        }
+
+        private static int Plus(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
