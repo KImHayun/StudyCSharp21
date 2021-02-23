35e83c0ee74947b0e4ecaf64cbdf3f5d1294838c
KimHayun <95-ha@naver.com>
2021-02-23 11:48:07 +0900

    조건문, 반복문

---
+++ chap05/Chap05App/Console/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (day)
+            {
+                case "월요일":
+                Console.Writeline("월요일");
+                    break;  
+
+            }
+        }
+    }
+}
