80f05330b2c7058188dfbb1d2c5780e75abd9542
KimHayun <95-ha@naver.com>
2021-02-25 13:38:16 +0900

    2월25일

---
+++ chap08/Chap08App/Chap08App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap08App
+{
+    interface ILogger
+    {
+
+        void WriteLog(string msg);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+
+            Console.WriteLine($"{ new DateTime.Now.ToShortDateString()} log : {msg}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLogger logger = new ConsoleLogger();
+            logger.WriteLog("로그출력!");
+        }
+    }
+}
