ec28e263026592b3ce1643ca97618ab59bd0a518
KimHayun <95-ha@naver.com>
2021-02-25 15:31:07 +0900

    2월25일

---
+++ chap09/Chap09App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap09App
+{ 
+    class MyClass
+{
+    public int value; // 0~120 값만 입력받는 멤버변수
+  
+        public MyClass (int Value)
+        {
+            this.value = value;
+
+        }
+        public void PrintValue()
+
+        {
+
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
