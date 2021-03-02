1fb4e712ffac9848cd41a9faa556f72919e81f4f
KimHayun <95-ha@naver.com>
2021-03-02 17:27:26 +0900

    3월2일

---
+++ chap13/Chap13App/DelegateChainApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateChainApp
+{
+    delegate void AllCalc(int x, int y); //대리자 선언
+    class Program
+    {
+        static void Plus(int a, int b) { Console.WriteLine($"a+b = {a + b}"); }
+        static void Minus(int a, int b) { Console.WriteLine($"a-b = {a - b}"); }
+        static void Multipul(int a, int b) { Console.WriteLine($"a*b= {a * b}"); }
+        static void Divide(int a, int b) { Console.WriteLine($"a/b= {a/b}"); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calulating!!");
+            AllCalc allCalc = Plus;
+            allCalc += Minus;
+            allCalc += Multipul;
+            allCalc += Divide;
+            allCalc(10, 5);
+
+            Console.WriteLine("곱셈 메서드 제거");
+            allCalc -= Multipul;
+            allCalc(10, 5);
+        }
+    }
+}
